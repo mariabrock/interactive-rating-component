a483a97c201f535cc9591a2bba126fee1d323b38
Maria Brock <brockm@ornl.giv>
2023-06-20 15:52:04 -0400

    working on the JS

---
+++ main.js
@@ -0,0 +1,16 @@
+{
+  'use strict'
+
+  console.log("this works")
+
+  function switchFunc() {
+    var ratingState = document.getElementById();
+    var thankYouState = document.getElementById();
+  
+    ratingState.style.display = (
+      mainFrameOne.style.display == "none" ? "block" : "none"); 
+    thankYouState.style.display = (
+      mainFrameTwo.style.display == "none" ? "block" : "none"); 
+  }
+
+}
