fea0d4fc348e9a527adf971cf2415d26b17ca8a1
Maria Brock <brockm@ornl.giv>
2023-07-28 12:11:42 -0400

    reworked css

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-{
-  'use strict'
+const ratingNumbers = document.getElementById("ratings");
+const submitBtn = document.getElementById("submit");
+const ratingCard = document.getElementById("rating-state");
+const thankYouCard = document.getElementById("thankyou-state");
+const selectionText = document.getElementById("selection-text");
 
   console.log("this works")
-
-  function switchFunc() {
-    var ratingState = document.getElementById();
-    var thankYouState = document.getElementById();
   
-    ratingState.style.display = (
-      mainFrameOne.style.display == "none" ? "block" : "none"); 
-    thankYouState.style.display = (
-      mainFrameTwo.style.display == "none" ? "block" : "none"); 
+  const onSubmit = (e) => {
+    if(e.target.classList.contains('btn-submit')) {
+      if (thankYouCard.style.display === 'none' &&  ratingCard.style.display === 'block') {
+        thankYouCard.style.visibility = 'visible';
+        ratingCard.style.visibility = 'hidden';
+      } else {
+        thankYouCard.style.visibility = 'hidden';
+        ratingCard.style.visibility = 'visible';
+      }
+    }
   }
-
-}
